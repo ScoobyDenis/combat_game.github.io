f312041fd585082678397062cc59fa17f5c61eb0
ScoobyDenis <95019939+ScoobyDenis@users.noreply.github.com>
2024-09-07 14:03:36 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ let tg = window.Telegram.WebApp;
 
 tg.expand()
 
-let btn = document.querySelector('#click')
+tg.MainButton.textColor = "#FFFFFF"
+tg.MainButton.color = "#2cab37"
 
+let btn = document.querySelector('#click')
+let btn1 = document.querySelector('#btn1')
+let btn2 = document.querySelector('#btn2')
+let item = ""
 btn.onclick = () => {
     if (tg.MainButton.isVisible) {
         tg.MainButton.hide()
@@ -12,3 +17,25 @@ btn.onclick = () => {
         tg.MainButton.show()
     }
 }
+btn1.onclick = () => {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide()
+    } else {
+        tg.MainButton.setText("Вы выбрали голову")
+        item = 'голова'
+        tg.MainButton.show()
+    }
+}
+btn2.onclick = () => {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide()
+    } else {
+        tg.MainButton.setText("Вы выбрали тело")
+        item = 'тело'
+        tg.MainButton.show()
+    }
+}
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+    tg.sendData(item)
+})
+
